87809c15042f418facb95364b82f6ffaa5109331
Ishika Knsal <ishikakasal05@gmail.com>
2024-12-12 17:47:44 +0530

    modified files

--- backend/server.js
+++ backend/server.js
@@ -8,6 +8,7 @@ const userRouter = require("./routes/user-route")
 const companyRouter = require("./routes/company-route")
 const jobRouter = require("./routes/job-route")
 const applicationRouter = require("./routes/application-route")
+const path = require("path")
 
 const corsOptions = {
     origin : "http://localhost:5173",
@@ -15,22 +16,24 @@ const corsOptions = {
   allowedHeaders: ['Content-Type', 'Authorization'],
     credentials : true,
 }
+
+const _dirname = path.resolve()
 app.use(express.json());
 app.use(express.urlencoded({extended : true}));
 app.use(cookieParser())
 app.use(cors(corsOptions))
 
 
-
-app.get("/", (req, res) => {
-    res.send("hello")
-})
-
 app.use("/api/v1/user", userRouter)
 app.use("/api/v1/company", companyRouter)
 app.use("/api/v1/job", jobRouter)
 app.use("/api/v1/application", applicationRouter)
 
+app.use(express.static(path.join(_dirname, "frontend/dist")))
+
+app.get("*", (_, res) => {
+    res.sendFile(path.resolve(_dirname, "frontend", "dist", "index.html"))
+})
 
 app.listen(process.env.PORT || 3000, () => {
     connectDb()
